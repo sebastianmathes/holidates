6022bf2e6d6ab2ab5fa1da3f4100d472d5c1e553
sebastianmathes <s.mathes@posteo.de>
2020-06-07 14:39:52 +0200

    sort output using lists

--- holidates.py
+++ holidates.py
@@ -4,6 +4,7 @@ from dateutil.easter import *
 from datetime import timedelta
 from datetime import date
 from typing import List
+from operator import itemgetter
 
 
 def calc_holidays(year: int = date.today().year) -> List:
@@ -22,30 +23,38 @@ def calc_holidays(year: int = date.today().year) -> List:
 
     TODO: Add option to get holidays for other states than Hessen
     """
-    # timedeltas for easter dependent holidays
-    deltas = [
-            ['Karfreitag', timedelta(days=-2)],
-            ['Ostermontag', timedelta(days=1)],
-            ['Himmelfahrt', timedelta(days=39)],
-            ['Pfingstmontag', timedelta(days=50)],
-            ['Fronleichnam', timedelta(days=60)]  # only in BW,BY,HE,NW,RP,SL
-            ]
-
     # dates for static holidays
     holidates = [
-            ['Neujahr', date(year, 1, 1)],
-            ['1ter Mai', date(year, 5, 1)],
-            ['Tag der dt. Einheit', date(year, 10, 3)],
-            ['1ter Weihnachtstag', date(year, 12, 25)],
-            ['2ter Weihnachtstag', date(year, 12, 26)]
+            {'Name': 'Neujahr', 'Datum': date(year, 1, 1)},
+            {'Name': '1ter Mai', 'Datum': date(year, 5, 1)},
+            {'Name': 'Tag der dt. Einheit','Datum': date(year, 10, 3)},
+            {'Name': '1ter Weihnachtstag', 'Datum': date(year, 12, 25)},
+            {'Name': '2ter Weihnachtstag', 'Datum': date(year, 12, 26)}
             ]
 
+    # timedeltas for easter dependent holidays
+    deltas = {
+            'Karfreitag': timedelta(days=-2),
+            'Ostermontag': timedelta(days=1),
+            'Himmelfahrt': timedelta(days=39),
+            'Pfingstmontag': timedelta(days=50),
+            'Fronleichnam': timedelta(days=60)  # only in BW,BY,HE,NW,RP,SL
+            }
+
     for k, v in deltas.items():
-        holidates[k] = easter(year) + v
+        holiday = {'Name' : k, 'Datum': easter(year) + v }
+        holidates.append(holiday)
+
+    # sort output by date
+    holidates = sorted(holidates, key=lambda dates : dates['Datum'])
     
     return holidates
 
 if __name__ == "__main__":
-    x = calc_holidays()
-    for k,v in x.items():
-        print(k,":",v)
+    holi = calc_holidays()
+
+    for day in holi:
+        print(day['Name'], ":", day['Datum'])
+
+
+
